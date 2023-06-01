c37ac131b0398b6985423099c881e3e3cf5d62b9
John Doe <johndoe@example.com>
2023-06-01 11:56:22 +0200

    Filtres fonctionnel

--- FrontEnd/index.js
+++ FrontEnd/index.js
@@ -1,44 +1,48 @@
+fetch("http://localhost:5678/api/works")
+  .then(response => response.json())
+  .then(works => {
+    displayWorks(works); 
 
-  fetch("http://localhost:5678/api/works")
-    .then(response => response.json())
-    .then(data => {
-      displayWorks(data); 
+    function displayWorks(works) {
+      const gallery = document.querySelector(".gallery");
+      gallery.innerHTML = "";
 
-      function displayWorks(works) {
-        const gallery = document.querySelector(".gallery");
-        gallery.innerHTML = "";
-      
-        works.forEach(work => {
-          const figure = document.createElement("figure");
-          const image = document.createElement("img");
-          image.src = work.imageUrl;
-          image.alt = work.title;
-          const figcaption = document.createElement("figcaption");
-          figcaption.textContent = work.title;
-          figure.appendChild(image);
-          figure.appendChild(figcaption);
-          gallery.appendChild(figure);
-        });
-      }
-      
-      fetch("http://localhost:5678/api/categories") 
-    .then(response => response.json())
-    .then(categories => {
-      const filtersContainer = document.getElementById("filtres");
-      
-      categories.forEach(category => {
-        const filterButton = document.createElement("button");
-        filterButton.textContent = category.name;
-        filterButton.dataset.categoryId = category.id;
-        filterButton.classList.add("filtres-button");
-        filterButton.addEventListener("click", function() {
-          const categoryId = this.dataset.categoryId;
-          fetchWorksByCategory(categoryId); 
-          updateActiveFilterButton(this);
-        });
-  
-        filtersContainer.appendChild(filterButton);
+      works.forEach(work => {
+        const figure = document.createElement("figure");
+        const image = document.createElement("img");
+        image.src = work.imageUrl;
+        image.alt = work.title;
+        const figcaption = document.createElement("figcaption");
+        figcaption.textContent = work.title;
+        figure.appendChild(image);
+        figure.appendChild(figcaption);
+        gallery.appendChild(figure);
       });
-    })
+    }
+
+    fetch("http://localhost:5678/api/categories") 
+      .then(response => response.json())
+      .then(categories => {
+        const filtersContainer = document.getElementById("filtres");
+
+        categories.forEach(category => {
+          const filterButton = document.createElement("button");
+          filterButton.textContent = category.name;
+          filterButton.dataset.categoryId = category.id;
+          filterButton.classList.add("filtres-button");
+          filterButton.addEventListener("click", function() {
+            const categoryId = this.dataset.categoryId;
+            const filteredWorks = works.filter(work => work.categoryId === parseInt(categoryId));
+            displayWorks(filteredWorks);
+            updateActiveFilterButton(this);
+          });
+
+          filtersContainer.appendChild(filterButton);
+        });
+      });
   });
 
+
+
+
+ 
