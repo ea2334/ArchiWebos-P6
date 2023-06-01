60cf142bd15c3b2b896bbdaaa44fd51752437ea3
John Doe <johndoe@example.com>
2023-06-01 03:13:34 +0200

    Ajout des filtres depuis l'API

--- FrontEnd/index.js
+++ FrontEnd/index.js
@@ -1,28 +1,44 @@
-fetch("http://localhost:5678/api/works")
-      .then(response => response.json())
-      .then(data => {
 
+  fetch("http://localhost:5678/api/works")
+    .then(response => response.json())
+    .then(data => {
+      displayWorks(data); 
+
+      function displayWorks(works) {
         const gallery = document.querySelector(".gallery");
-        data.forEach(work => {
-
-        const figure = document.createElement("figure");
-
-        const image = document.createElement("img");
-        image.src = work.imageUrl;
-        image.alt = work.title;
-
-        const figcaption = document.createElement("figcaption");
-        figcaption.textContent = work.title;
-        figure.appendChild(image);
-        figure.appendChild(figcaption);
-        gallery.appendChild(figure);
+        gallery.innerHTML = "";
+      
+        works.forEach(work => {
+          const figure = document.createElement("figure");
+          const image = document.createElement("img");
+          image.src = work.imageUrl;
+          image.alt = work.title;
+          const figcaption = document.createElement("figcaption");
+          figcaption.textContent = work.title;
+          figure.appendChild(image);
+          figure.appendChild(figcaption);
+          gallery.appendChild(figure);
         });
-      })
-
-
+      }
+      
+      fetch("http://localhost:5678/api/categories") 
+    .then(response => response.json())
+    .then(categories => {
+      const filtersContainer = document.getElementById("filtres");
+      
+      categories.forEach(category => {
+        const filterButton = document.createElement("button");
+        filterButton.textContent = category.name;
+        filterButton.dataset.categoryId = category.id;
+        filterButton.classList.add("filtres-button");
+        filterButton.addEventListener("click", function() {
+          const categoryId = this.dataset.categoryId;
+          fetchWorksByCategory(categoryId); 
+          updateActiveFilterButton(this);
+        });
   
-  
-
-
-
+        filtersContainer.appendChild(filterButton);
+      });
+    })
+  });
 
