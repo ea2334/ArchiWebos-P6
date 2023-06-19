6dde079c30c2bdc8946d878fb40b75214a5e96c6
John Doe <johndoe@example.com>
2023-06-19 21:01:15 +0200

    .

--- FrontEnd/assets/javascript/modal.js
+++ FrontEnd/assets/javascript/modal.js
@@ -24,3 +24,27 @@ const closeModal = function (e) {
   document.querySelectorAll('.js-modal').forEach(a => {
     a.addEventListener('click', openModal);
   })
+
+  
+  fetch("http://localhost:5678/api/works")
+  .then(response => response.json())
+  .then(works => {
+    displayWorks(works); 
+
+    function displayWorks(works) {
+      const gallery = document.querySelector(".gallery");
+      gallery.innerHTML = "";
+
+      works.forEach(work => {
+        const figure = document.createElement("figure");
+        const image = document.createElement("img");
+        image.src = work.imageUrl;
+        image.alt = work.title;
+        const figcaption = document.createElement("figcaption");
+        figcaption.textContent = work.title;
+        figure.appendChild(image);
+        figure.appendChild(figcaption);
+        gallery.appendChild(figure);
+      });
+    }
+});
